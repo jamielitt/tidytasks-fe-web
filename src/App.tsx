a582570c22fa262207f359caa3b73ae6efaba30e
Jamie Little <jamielittle@Jamies-Mac-mini.local>
2025-08-23 15:36:21 +0100

    Updated to remove .tsx estension reliance in include

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
 
 // Need to remove tsx extension from here, find out why
+// Update - when everything is named as tsx it is no longer needed
+// keeping this comment here for future reference
 import Header from './components/ui/header'
 
 function App() {

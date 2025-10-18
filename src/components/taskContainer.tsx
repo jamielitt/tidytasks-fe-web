130cbc7bd14b6d6314f837ac91f55db3f947721a
Jamie Little <jamie.littledv@gmail.com>
2025-10-19 00:35:43 +0100

    Added skeleton for adding tasks

--- src/components/taskContainer.tsx
+++ src/components/taskContainer.tsx
@@ -24,10 +24,14 @@ export default function TaskContainer() {
         console.log("Task Clicked");
     }
 
+    const onTaskAdd = () => {
+        console.log("Adding a task");
+    }
+
     return(
         <div className="flex gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10relative flex flex-col rounded-lg bg-white shadow-sm border border-slate-200">
             <ul className="flex min-w-[240px] flex-col gap-1 p-1.5">
-                <TaskList taskList={listOfTasks.taskList} onTaskClick={onTaskClick}/>
+                <TaskList taskList={listOfTasks.taskList} onTaskClick={onTaskClick} onTaskAdd={onTaskAdd}/>
             </ul>
         </div>
     )
--- src/components/taskList.tsx
+++ src/components/taskList.tsx
@@ -2,19 +2,24 @@ import { task } from "../models/tasks";
 
 type TaskListProps = {
   taskList: Array<task>,
-  onTaskClick: () => void
+  onTaskClick: () => void,
+  onTaskAdd: () => void
 };
 
-export function TaskList({ taskList, onTaskClick }: TaskListProps) {
+export function TaskList({ taskList, onTaskClick, onTaskAdd }: TaskListProps) {
+    const taskStyle = "text-slate-800 flex w-full items-center rounded-md p-3 transition-all hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100";
     return ( 
         <>
-        {taskList.map(t => (          
-            <li onClick={onTaskClick} role="button" className="text-slate-800 flex w-full items-center rounded-md p-3 transition-all hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100">
+        {taskList.length > 0 && taskList.map(t => (          
+            <li role="button" className={taskStyle} onClick={onTaskClick}>
                 <span>{t.name}</span>
                 <span>{t.description}</span>
             </li>
             )
         )}
+        {taskList.length == 0 &&
+            <div className={taskStyle} onClick={onTaskAdd}>You can add your first task by clicking here</div>
+        }
         </>
     )
 }

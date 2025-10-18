566bf8ff040fb08135d02ba1374776a1c419ef68
jamielitt <jamie.littledv@gmail.com>
2025-10-18 16:56:24 +0100

    Slight layout tidyup using Tailwind (#5)

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,7 +4,7 @@ import { AccountContext } from "../../providers/account/useAccountProvider";
 export default function Header() {
   const context = useContext(AccountContext);
   return (
-    <header className="text-3xl font-bold underline">
+    <header className="flex gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
         <p>Header Component</p>
         <p>User: {context.currentUser.Name}</p>        
     </header>

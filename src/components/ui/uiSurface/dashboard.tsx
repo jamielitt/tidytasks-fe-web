1c775e6062bd14233f3b2fc2702ce21d96cdff8f
Jamie Little <jamie.littledv@gmail.com>
2025-11-01 12:05:33 +0000

    Refactoring based upon PR AB#8

--- src/components/ui/uiSurface/dashboard.tsx
+++ src/components/ui/uiSurface/dashboard.tsx
@@ -1,12 +1,13 @@
 import { uiSurfaceProps } from "../interfaces/surfaceProps";
+import { basicContainerStyle } from "../styles/base";
 
 export default function Dashboard ({ onTaskContainerClick }: uiSurfaceProps) {
     return (
-        <div className="flex gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10relative flex flex-col rounded-lg bg-white shadow-sm border border-slate-200">
+        <div className={basicContainerStyle}>
             <span>Dashboard Commponent goes here</span>
             <button type="button" 
                 onClick={onTaskContainerClick} 
-                className="flex gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10relative flex flex-col rounded-lg bg-white shadow-sm border border-slate-200">Display Tasklists</button>
+                className={basicContainerStyle}>Display Tasklists</button>
         </div>
     );
 };
--- src/components/ui/uiSurface/taskContainer.tsx
+++ src/components/ui/uiSurface/taskContainer.tsx
@@ -1,5 +1,6 @@
 import useTasks from "../../../hooks/tasks/useTasks";
 import { uiSurfaceProps } from "../interfaces/surfaceProps";
+import { basicContainerStyle } from "../styles/base";
 import { TaskList } from "./taskList";
 
 export default function TaskContainer({ onDashboardClick }: uiSurfaceProps) {
@@ -8,13 +9,13 @@ export default function TaskContainer({ onDashboardClick }: uiSurfaceProps) {
 
     return(
         <>
-        <div className="flex gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10relative flex flex-col rounded-lg bg-white shadow-sm border border-slate-200">
+        <div className={basicContainerStyle}>
             <ul className="flex min-w-[240px] flex-col gap-1 p-1.5">
                 <TaskList taskList={listOfTasks.taskList} onTaskClick={onTaskClick} onTaskAdd={onTaskAdd}/>
             </ul>
         </div>
         <div>
-            <button className="flex gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10relative flex flex-col rounded-lg bg-white shadow-sm border border-slate-200" onClick={onDashboardClick}>Return to Dashboard</button>
+            <button className={basicContainerStyle} onClick={onDashboardClick}>Return to Dashboard</button>
         </div>
         </>
     )

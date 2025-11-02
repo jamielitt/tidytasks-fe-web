2490026be846411375af965def1f1f0e6f119e6c
Jamie Little <jamie.littledv@gmail.com>
2025-11-02 20:59:44 +0000

    Added surface for Add Task and improved navigation

--- src/components/ui/uiSurface.tsx
+++ src/components/ui/uiSurface.tsx
@@ -2,15 +2,18 @@ import Dashboard from "./uiSurface/dashboard";
 import TaskContainer from "./uiSurface/taskContainer";
 import { currentSelection } from "../enums/uiSurface";
 import useUiSurface from "../../hooks/uiSurface/useUiSurface";
+import AddTask from "./uiSurface/addTask";
 
 export default function UISurface() {
 
-    const {activeSurface, onTaskContainerClicked, onDashboardClick} = useUiSurface();
+    const {activeSurface, displayTaskList, 
+        displayDashboard, displayAddTask, goBack} = useUiSurface();
 
     return(
         <>
-        {activeSurface == currentSelection.DASHBOARD && <Dashboard onTaskContainerClick={onTaskContainerClicked}/>}
-        {activeSurface == currentSelection.TASK_CONTAINER && <TaskContainer onDashboardClick={onDashboardClick}/>}
+        {activeSurface === currentSelection.DASHBOARD && <Dashboard onTaskContainerClick={displayTaskList} onAddTaskClick={displayAddTask}/>}
+        {activeSurface === currentSelection.TASK_CONTAINER && <TaskContainer onDashboardClick={displayDashboard} onAddTaskClick={displayAddTask}/>}
+        {activeSurface === currentSelection.ADD_TASK && <AddTask onCancelClick={goBack}/>}
         </>
     );
 };
